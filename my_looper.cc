71862d6ca68f235ac6ac486399d820a93d5c194b
Philip Chang <philip@ucsd.edu>
2025-12-30 19:22:05 -0500

    update

--- my_looper.cc
+++ my_looper.cc
@@ -3,9 +3,9 @@
 int main(int argc, char** argv)
 {
     // Get the file names to process
-    // std::vector<std::string> files = {"/blue/avery/p.chang/nanoaod/mc/RunIISummer20UL18NanoAODv9/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/230000/1A59D67A-325A-E742-BFFE-2FE5F69EB01B.root"};
+    std::vector<std::string> files = {"/blue/avery/p.chang/nanoaod/mc/RunIISummer20UL18NanoAODv9/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/230000/1A59D67A-325A-E742-BFFE-2FE5F69EB01B.root"};
     // std::vector<std::string> files = {"/blue/avery/p.chang/nanoaod/mc/RunIISummer20UL18NanoAODv9/WWZ_4F_TuneCP5_13TeV-amcatnlo-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1_ext1-v2/2500000/2A0EB266-E607-8646-971D-45D5F962C147.root"};
-    std::vector<std::string> files = {"/blue/avery/p.chang/nanoaod/mc/RunIISummer20UL18NanoAODv9/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/40000/8718B61F-4D6B-6B4C-AA07-A29ADBDC7FD8.root"};
+    // std::vector<std::string> files = {"/blue/avery/p.chang/nanoaod/mc/RunIISummer20UL18NanoAODv9/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/40000/8718B61F-4D6B-6B4C-AA07-A29ADBDC7FD8.root"};
     // std::vector<std::string> files = {"/blue/avery/p.chang/nanoaod/mc/RunIISummer20UL18NanoAODv9/TTZToLL_TuneCP5_13TeV_amcatnlo-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v3/2830000/91815FD7-FD43-7444-92BB-D0CB29A30212.root"};
 
     // Open the files into a RDataFrame
@@ -33,8 +33,8 @@ int main(int argc, char** argv)
 
     // Print
     // (*collection_building.Display({"GoodGenPart_pdgId", "GoodGenPart_status", "GoodGenPart_statusFlags", "GoodGenPart_pt"}, 9, 200)).Print();
-    // (*collection_building.Display({"GoodGenPart_pdgId", "GoodGenPart_status", "GoodGenPart_statusFlags", "GoodGenPart_pt"}, 9, 200)).Print();
-    (*collection_building.Display({"GoodLepton_goodGenPartIdx", "GoodLepton_goodGenPartDr", "GoodLepton_pt", "GoodGenPart_pt"}, 9, 200)).Print();
+    (*collection_building.Display({"GoodGenPart_pdgId", "GoodGenPart_status", "GoodGenPart_statusFlags", "GoodGenPart_pt"}, 23, 200)).Print();
+    // (*collection_building.Display({"GoodLepton_goodGenPartIdx", "GoodLepton_goodGenPartDr", "GoodLepton_pt", "GoodGenPart_pt"}, 23, 200)).Print();
 
     // Output
     auto cols_to_write = RdfUtil::SelectColumnNames(collection_building, {"Good*"});
